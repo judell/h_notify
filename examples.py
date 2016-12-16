95351874ae1cc394b6d25a9f1cd24dee3fa2d695
judell <jon@jonudell.info>
2016-12-16 09:45:07 -0800

    add email and rss examples

--- examples.py
+++ examples.py
@@ -6,6 +6,9 @@ default_hook = 'https://hooks.slack.com/services/T03...yzy' # see https://YOUR_V
 
 while True:
     try:
+
+        # slack recipes
+
         # notify when an url in a list of urls of interest to the team is annotated
         urls = ['http://example.com', 'https://hypothes.is/blog/test-our-new-canvas-app-prototype/']
         for url in urls:
@@ -22,7 +25,18 @@ while True:
         # notify when there's an annotation with a specified tag
         notify_slack_tag_activity(tag='nextprez', token=default_token, pickle='nextprez', channel='test', hook=default_hook)
 
+
+        # rss recipes 
+
+        notify_rss_group_activity(group='8gk9i7VV', groupname='Anyone Can Join', token=default_token, pickle='8gk9i7VV')
+
+        # email recipes
+
+        notify_email_tag_activity(tag='futuoer', token=default_token, pickle='futuoer', smtp=default_smtp_server, sender=default_email_sender, sender_password=default_email_password, recipient='nobody@example.com')
+
         print ( 'sleeping' )
         time.sleep(60 * 30)  # wait 30 min to be polite to the hypothesis api
     except:
         print ( traceback.print_exc() )
+
+
