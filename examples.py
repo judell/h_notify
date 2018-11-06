74e5923cb9f61d9414989182b9f946a5360cd040
Jon Udell <jon@jonudell.info>
2018-11-06 15:54:24 -0800

    Update examples.py

--- examples.py
+++ examples.py
@@ -4,8 +4,8 @@ import time
 default_token = '687...3df5' # hypothesis api token for a user who is a member of all monitored groups
 default_hook = 'https://hooks.slack.com/services/T03...yzy' # see https://YOUR_VANITY_NAME.slack.com/apps/manage/custom-integrations)
 
-default_smtp_server = 'smtp.example.com'
-default_email_sender = 'hypothesis.notifier@example.com.com'
+default_smtp_server = 'smtp.example.com' 
+default_email_sender = 'hypothesis.notifier@example.com'
 default_email_password = 'q1...Nm'
 
 while True:
